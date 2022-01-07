fe4339a7fca48f7db00274646b2ac09973139585
vahid <vahid.muradov31@gmail.com>
2022-01-07 15:14:35 +0400

    ders2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// DRY - do not reapead yourself
+
+
+
+
+
+
+
+
+//var ad = "vahid";
+//for(var i = 5 - 1; i >= 0; i++)
+//{
+//    Console.WriteLine(ad);
+//}
+
+
+string[] telebeler =
+{
+    "mehemmed" , "vahid", "aytac", "gunel", "resad"
+};
+for(int i= 0; i<telebeler.Length; i++)
+{
+    Console.WriteLine(telebeler[i]);
+}
+
+
